7048d72f320aaa90e4c272b96fb4e132b70f46d1
Anthony <antoca29@gmail.com>
2021-05-09 05:22:48 -0600

    Primer commit de Anthony

---
+++ cmake-build-debug/CMakeFiles/Proyecto2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proyecto2.dir/main.cpp.o"
+  "Proyecto2"
+  "Proyecto2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proyecto2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
