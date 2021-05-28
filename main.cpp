136ede78383369c89e19907e8f93a2be7b01d8e9
David <divad0907@gmail.com>
2021-05-28 01:51:59 -0600

    Updadate GENETIC DISPLAY

--- main.cpp
+++ main.cpp
@@ -9,10 +9,10 @@
 int main() {
     //PathfindingA * pathfindingA = new PathfindingA();
     //pathfindingA->getPath(6,7);
-    //Board * board = new Board();
-    //board->getPathPlayer();
-    GeneticDisplay geneticD;
-    geneticD.rungenetic();
+    Board * board = new Board();
+    board->getPathPlayer();
+    //GeneticDisplay geneticD;
+    //geneticD.rungenetic();
 //    RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
 //    CircleShape shape(100.f);
 //    shape.setFillColor(Color::Green);
