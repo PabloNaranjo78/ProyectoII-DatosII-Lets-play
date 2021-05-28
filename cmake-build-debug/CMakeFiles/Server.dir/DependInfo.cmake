d140907ffc4184a90247d8438c2a715cfe443703
David <divad0907@gmail.com>
2021-05-28 01:52:28 -0600

    Updadate GENETIC DISPLAY

---
+++ cmake-build-debug/CMakeFiles/Server.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/david/Documents/Proyecto2Complete/ProyectoII-DatosII-Lets-play/Server/Server.cpp" "/home/david/Documents/Proyecto2Complete/ProyectoII-DatosII-Lets-play/cmake-build-debug/CMakeFiles/Server.dir/Server/Server.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../SFML_INCLUDE_DIR"
+  "../AlgoritmoG"
+  "../Graphics"
+  "../ImageLoading"
+  "../ImageCutting"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
